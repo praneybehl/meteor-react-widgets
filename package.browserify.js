93eb1e9e73938dc1bff7fc16323ef5763d242016
Praney Behl <praneybehl@gmail.com>
2015-11-05 18:15:13 +1100

    Browserify export and config son

---
+++ package.browserify.js
@@ -0,0 +1,8 @@
+ReactWidgets = require('react-widgets');
+DropdownList = ReactWidgets.DropdownList;
+Combobox = ReactWidgets.Combobox;
+NumberPicker = ReactWidgets.NumberPicker;
+Multiselect = ReactWidgets.Multiselect;
+SelectList = ReactWidgets.SelectList;
+Calendar = ReactWidgets.Calendar;
+DateTimePicker = ReactWidgets.DateTimePicker;
